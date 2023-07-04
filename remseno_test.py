d3272a677d5b9126ccda9202a997b4818a027615
kaplangorde <kaplangorde@gmail.com>
2023-07-04 16:38:34 +0300

    WORKING YAY!

--- remseno_test.py
+++ remseno_test.py
@@ -9,21 +9,25 @@ c = Coords(coordinate_file_path, x_col='X', y_col='Y', label_col='class',
 
 
 from remseno import *
-drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waeldi_2022_multispectral_noreflectance.tif'
+drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waldi_july.tif'
 o = Image()
 o.load_image(image_path=drone_ortho)
 
-c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:4326", plot=True)
+c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:32632", plot=True)
 
 c.plot_on_image(image=o, band=1)
-o.plot_multi_bands(bands=[1, 2, 3], downsample=10, show_plot=True)
+o.plot_multi_bands(bands=[1, 2, 3], downsample=1, show_plot=True)
+
 
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier(random_state=0)
 
+bands = [o.image.read(b) for b in [1, 2, 3, 4, 5, 6, 7, 8]]
+
 ml = ML()
-ml_df = ml.train_ml(clf, image=o, bands=[1, 2, 3], coords=c, normalise=True, max_pixel_padding=1)
-ml_df.to_csv('C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/nov.csv')
+df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=20, test_percent=20,
+                max_pixel_padding=2, normalise=False)
+df.to_csv('test_pred.csv')
 
 
 
