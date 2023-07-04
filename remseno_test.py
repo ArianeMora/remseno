1af41949d2169c9819c791a43866e69a3869e237
kaplangorde <kaplangorde@gmail.com>
2023-07-04 14:20:04 +0300

    Update remseno_test.py

--- remseno_test.py
+++ remseno_test.py
@@ -9,14 +9,14 @@ c = Coords(coordinate_file_path, x_col='X', y_col='Y', label_col='class',
 
 
 from remseno import *
-drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waldi_nov.tif'
+drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waeldi_2022_multispectral_noreflectance.tif'
 o = Image()
 o.load_image(image_path=drone_ortho)
 
-c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:32632", plot=True)
+c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:4326", plot=True)
 
 c.plot_on_image(image=o, band=1)
-o.plot_multi_bands(bands=[1, 2, 3], downsample=1, show_plot=True)
+o.plot_multi_bands(bands=[1, 2, 3], downsample=10, show_plot=True)
 
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier(random_state=0)
