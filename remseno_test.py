df8e7cbe00c9ca04dc6a4c596f279a0aa14ac3de
kaplangorde <kaplangorde@gmail.com>
2023-07-10 11:58:55 +0300

    works_ortho
    works with the waeldi ortho

--- remseno_test.py
+++ remseno_test.py
@@ -9,11 +9,11 @@ c = Coords(coordinate_file_path, x_col='X', y_col='Y', label_col='class',
 
 
 from remseno import *
-drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waldi_july.tif'
+drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waeldi.tif'
 o = Image()
 o.load_image(image_path=drone_ortho)
 
-c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:32632", plot=True)
+c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:21781", plot=True)
 
 c.plot_on_image(image=o, band=1)
 o.plot_multi_bands(bands=[1, 2, 3], downsample=1, show_plot=True)
@@ -22,7 +22,7 @@ o.plot_multi_bands(bands=[1, 2, 3], downsample=1, show_plot=True)
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier(random_state=0)
 
-bands = [o.image.read(b) for b in [1, 2, 3, 4, 5, 6, 7, 8]]
+bands = [o.image.read(b) for b in [1, 2, 3, 4]]
 
 ml = ML()
 df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=20, test_percent=20,
