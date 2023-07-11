e34ecc3351b603fd93198449a84557aa9e730954
kaplangorde <kaplangorde@gmail.com>
2023-07-11 16:50:31 +0300

    asbestos_works

--- remseno_test.py
+++ remseno_test.py
@@ -3,20 +3,20 @@ import numpy as np
 np.seterr(divide='ignore', invalid='ignore')
 
 
-coordinate_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Waeldi_Adults_genotyped2.csv'
+coordinate_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/for_remseno_asbestos.csv'
 c = Coords(coordinate_file_path, x_col='X', y_col='Y', label_col='class',
-                   id_col='id', sep=',', class1='Sylvatica', class2='Orientalis', crs="EPSG:21781")
+                   id_col='id', sep=',', class1='n', class2='o', crs="EPSG:32634")
 
 
 from remseno import *
-drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/waeldi.tif'
+drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/rgb_sk.tif'
 o = Image()
 o.load_image(image_path=drone_ortho)
 
-c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:21781", plot=True)
+c.transform_coords(tree_coords="EPSG:32634", image_coords="EPSG:32634", plot=True)
 
 c.plot_on_image(image=o, band=1)
-o.plot_multi_bands(bands=[4, 2, 3], downsample=10, show_plot=True)
+o.plot_multi_bands(bands=[1, 2, 3], downsample=10, show_plot=True)
 
 
 from sklearn.tree import DecisionTreeClassifier
@@ -26,7 +26,7 @@ bands = [o.image.read(b) for b in [1, 2, 3, 4]]
 
 ml = ML()
 df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=30, test_percent=30,
-                max_pixel_padding=10, normalise=False)
+                max_pixel_padding=1, normalise=False)
 df.to_csv('test_pred.csv')
 
 
