9ed1a218bc9dc959d82e4d19758d502170136f97
kaplangorde <kaplangorde@gmail.com>
2023-07-12 16:18:41 +0300

    .

--- remseno_test.py
+++ remseno_test.py
@@ -16,7 +16,7 @@ o.load_image(image_path=drone_ortho)
 c.transform_coords(tree_coords="EPSG:32634", image_coords="EPSG:32634", plot=True)
 
 c.plot_on_image(image=o, band=1)
-o.plot_multi_bands(bands=[1, 2, 3], downsample=10, show_plot=True)
+o.plot_multi_bands(bands=[1, 2, 3], downsample=1, show_plot=True)
 
 
 from sklearn.tree import DecisionTreeClassifier
@@ -25,7 +25,7 @@ clf = DecisionTreeClassifier(random_state=0)
 bands = [o.image.read(b) for b in [1, 2, 3, 4]]
 
 ml = ML()
-df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=30, test_percent=30,
+df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=20, test_percent=20,
                 max_pixel_padding=1, normalise=False)
 df.to_csv('test_pred.csv')
 
