6872d28a1983db1bb1b5efd8229663bd8e51a202
kaplangorde <kaplangorde@gmail.com>
2023-10-25 14:27:14 +0300

    Revert "2510_working_version_Allen"
    This reverts commit 49bd23f43c582446a0f40cbb3ba435e3ece8872a.

--- remseno_test.py
+++ remseno_test.py
@@ -4,40 +4,31 @@ import numpy as np
 np.seterr(divide='ignore', invalid='ignore')
 
 
-coordinate_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Allenwiller_ACp_Coordinates_UTM32N_32632.csv'
+coordinate_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Allenwiller_ACp_GPSdata_WGS84_epsg4326_final.csv'
 c = Coords(coordinate_file_path, x_col='X', y_col='Y', label_col='class',
                    id_col='id', sep=',', class1='Sylvatica', class2='Orientals', crs="EPSG:4326")
 
 
 from remseno import *
-drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Allen_june.tif'
+drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/allenwiller_2021_ortho_PetraRefelectance.tif'
 o = Image()
 o.load_image(image_path=drone_ortho)
 
-c.transform_coords(tree_coords="EPSG:32632", image_coords="EPSG:32632", plot=True)
-
-# After you have performed the transformation, you can save the DataFrame to a new CSV file.
-#output_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Waeldi_Adults_transformed_32632.csv'  # Specify the path for the output file.
-
-# Assuming that 'c.df' contains the transformed data
-#c.df.to_csv(output_file_path, index=False)  # Save the DataFrame to a CSV file, excluding the index.
-
-# Optionally, you can print a message to confirm that the file has been saved.
-#print(f"Transformed coordinates saved to {output_file_path}")
+c.transform_coords(tree_coords="EPSG:4326", image_coords="EPSG:4326", plot=True)
 
 c.plot_on_image(image=o, band=1)
-o.plot_multi_bands(bands=[1, 2, 3], downsample=5, show_plot=True)
+o.plot_multi_bands(bands=[1, 2, 3], downsample=10, show_plot=True)
 
 
-from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1, random_state=42)
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(random_state=0)
 
-bands = [o.image.read(b) for b in [2, 3, 4, 5, 6, 7]]
+bands = [o.image.read(b) for b in [1, 2, 3, 4]]
 
 ml = ML()
-df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=40, test_percent=40,
-                max_pixel_padding=2, normalise=True)
-
+df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=30, test_percent=30,
+                max_pixel_padding=3, normalise=False)
+df.to_csv('test_pred.csv')
 
 
 
--- remseseno_test_PS.py
+++ remseseno_test_PS.py
@@ -4,16 +4,31 @@ import numpy as np
 np.seterr(divide='ignore', invalid='ignore')
 
 
-coordinate_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Waeldi_Adults_genotyped.csv'
-c = Coords(coordinate_file_path, x_col='Y', y_col='X', label_col='class',
-                   id_col='id', sep=',', class1='Sylvatica', class2='Orientals', crs="EPSG:21781")
-
-c.transform_coords(tree_coords="EPSG:21781", image_coords="EPSG:4326", plot=True)
+coordinate_file_path = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/Allenwiller_ACp_GPSdata_WGS84_epsg4326_final.csv'
+c = Coords(coordinate_file_path, x_col='X', y_col='Y', label_col='class',
+                   id_col='id', sep=',', class1='Sylvatica', class2='Orientals', crs="EPSG:4326")
 
 
+from remseno import *
+drone_ortho = 'C:/Users/Gorde/Documents/GitHub/remhybmon/data/public_data/allenwiller_2021_ortho_PetraRefelectance.tif'
+o = Image()
+o.load_image(image_path=drone_ortho)
+
+c.transform_coords(tree_coords="EPSG:4326", image_coords="EPSG:4326", plot=True)
+
+c.plot_on_image(image=o, band=1)
+o.plot_multi_bands(bands=[1, 2, 3], downsample=10, show_plot=True)
 
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(random_state=0)
 
+bands = [o.image.read(b) for b in [1, 2, 3, 4]]
+
+ml = ML()
+df = ml.train_ml(clf, image=o, coords=c, image_bands=bands, validation_percent=30, test_percent=30,
+                max_pixel_padding=3, normalise=False)
+df.to_csv('test_pred.csv')
 
 
 
